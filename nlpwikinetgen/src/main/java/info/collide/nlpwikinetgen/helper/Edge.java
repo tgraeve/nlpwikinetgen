a1fa9787a362042a20971d480069d32a1866f566
Tobias Graeve <tobias.graeve@gmail.com>
2017-03-06 16:56:56 +0100

    implemented new edge class to get new parameters into edges.

---
+++ nlpwikinetgen/src/main/java/info/collide/nlpwikinetgen/helper/Edge.java
@@ -0,0 +1,41 @@
+package info.collide.nlpwikinetgen.helper;
+
+public class Edge {
+	
+	String type;
+	int source;
+    int destination;
+
+    public Edge(String type, int source, int destination) {
+    	this.type = type;
+        this.source = source;
+        this.destination = destination;
+    }
+
+    public String getType() {
+    	return type;
+    }
+    
+    public int getSource() {
+        return source;
+    }
+
+    public int getDestination() {
+        return destination;
+    }
+    
+    public boolean equals(Object o) {
+        boolean equals = false;
+        if (o instanceof Edge) {
+             Edge sp = (Edge) o;
+             if (getType()==sp.getType() && getSource()==sp.getSource() && getDestination()==sp.getDestination()) {
+                 equals = true;
+             }
+        }
+        return equals;
+    }
+    
+    public int hashCode() {
+        return ("<" + getType() + ">,<" + getSource() + ">,<" + getDestination() + ">").hashCode();
+    }
+}
