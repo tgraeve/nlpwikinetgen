93e479a50b6abe1b10c79afd512a4511fef99c4b
Tobias Graeve <tobias.graeve@gmail.com>
2017-05-11 19:22:16 +0200

    first tests.

--- nlpwikinetgen/src/main/java/info/collide/nlpwikinetgen/helper/RDDBuilder.java
+++ nlpwikinetgen/src/main/java/info/collide/nlpwikinetgen/helper/RDDBuilder.java
@@ -2,10 +2,27 @@ package info.collide.nlpwikinetgen.helper;
 
 import java.util.List;
 
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.graphx.EdgeRDD;
+import org.apache.spark.graphx.VertexRDD;
+
 public class RDDBuilder {
 	
-	public  nodesToRDD(List<Node> nodes) {
+	JavaSparkContext sc = new JavaSparkContext();
+	
+	public EdgeRDD edgesToRDD(List<Edge> edges) {
 		
+		JavaRDD<Edge> rdd = sc.parallelize(edges);
+		EdgeRDD erdd = EdgeRDD.fromEdges(arg0, arg1, arg2);
+		return erdd;
+	}
+	
+	public VertexRDD<Node> nodesToRDD(List<Node> nodes) {
+		JavaRDD<Node> rdd = sc.parallelize(nodes);
+		VertexRDD vrdd = VertexRDD.fro
+		
+		return 
 	}
 
 }
