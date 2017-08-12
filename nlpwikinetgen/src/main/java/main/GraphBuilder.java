bbc215325ceb209545ffabe2d5af2bdac12e48fc
Tobias Graeve <tobias.graeve@gmail.com>
2017-08-12 04:14:58 +0200

    replaced all list iterations by spark joining approaches.

--- nlpwikinetgen/src/main/java/main/GraphBuilder.java
+++ nlpwikinetgen/src/main/java/main/GraphBuilder.java
@@ -5,21 +5,18 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
-import org.apache.spark.api.java.function.ForeachFunction;
-import org.apache.spark.api.java.function.Function;
+
+import static org.apache.spark.sql.functions.*;
 import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Encoder;
-import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.DataType;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
-import org.apache.spark.storage.StorageLevel;
-import org.apache.hadoop.yarn.webapp.hamlet.Hamlet.COL;
 import org.apache.log4j.*;
 import org.graphframes.GraphFrame;
 import info.collide.nlpwikinetgen.helper.GMLWriter;
@@ -86,6 +83,7 @@ public class GraphBuilder implements Serializable {
 			Dataset<Row> minorNodes = allNodes.join(minorNodeIds, allNodes.col("id").equalTo(minorNodeIds.col("id"))).drop(minorNodeIds.col("id"));
 			Dataset<Row> majorNodes = allNodes.except(minorNodes);
 			majorNodes.cache();
+			List<Node> majorNodeList = majorNodes.javaRDD().map(r -> new Node(r.getString(0),r.getString(1))).collect();
 			
 			//edges of type "link" where neither source nor destination are deleted can be kept
 			Dataset<Row> allLinks = dfEdges.filter("type = 'link'").join(dfNodes, dfEdges.col("src").equalTo(dfNodes.col("id"))).drop("id", "pageid").dropDuplicates(); //sort out links from foreign sites (where no nodes in data are existent)
@@ -93,30 +91,17 @@ public class GraphBuilder implements Serializable {
 			
 			Dataset<Row> unaffectedLinks = allLinks.join(majorNodes, allLinks.col("src").equalTo(majorNodes.col("id"))).drop("id","pageid");
 			unaffectedLinks = unaffectedLinks.join(majorNodes, unaffectedLinks.col("dst").equalTo(majorNodes.col("id"))).drop("id", "pageid");
-			
-			List<Edge> unaffectedLinksList = unaffectedLinks.javaRDD().map(r -> new Edge(r.getString(1),r.getString(0),r.getString(2))).collect(); //mind order of columns is not order of native node class!
-//			System.out.println(allLinks.count() +" ###" + unaffectedLinksList.size());
+						
 			Dataset<Row> affectedLinks = allLinks.except(unaffectedLinks);
 			System.out.println("reached");
 			
-			Dataset<Row> joined = affectedLinks.join(allNodes, affectedLinks.col("src").equalTo(allNodes.col("id"))).drop("id");
-			joined.printSchema();
-			System.out.println(joined.count());
-			joined = joined.join(majorNodes, joined.col("pageId").equalTo(majorNodes.col("pageId"))).drop("pageId").drop(majorNodes.col("pageId"));
-			joined.printSchema();
-			joined = joined.filter(x -> Integer.parseInt(x.getString(1)) > Integer.parseInt(x.getString(3))).withColumn("id", joined.col("id").cast("int"));
-			joined.printSchema();
-			joined.show();
-			joined = joined.groupBy("dst","src","type").max("id");
-			joined.printSchema();
-			joined.show();
-			System.out.println(joined.count());
-			
+			Dataset<Row> corrLinkEdges = affectedLinks.join(allNodes, affectedLinks.col("src").equalTo(allNodes.col("id"))).drop("id");
+			System.out.println(corrLinkEdges.count());
+			corrLinkEdges = corrLinkEdges.join(majorNodes, corrLinkEdges.col("pageId").equalTo(majorNodes.col("pageId"))).drop("pageId").drop(majorNodes.col("pageId"));
+			corrLinkEdges = corrLinkEdges.filter(x -> Integer.parseInt(x.getString(1)) > Integer.parseInt(x.getString(3))).withColumn("id", corrLinkEdges.col("id").cast("int"));
+			corrLinkEdges = corrLinkEdges.groupBy("dst","src","type").max("id");
+			corrLinkEdges = corrLinkEdges.drop("src").withColumn("src", corrLinkEdges.col("max(id)").cast("string")).select("dst","src","type");
 
-			
-			
-			List<Edge> affectedLinkEdges = affectedLinks.javaRDD().map(r -> new Edge(r.getString(1), r.getString(0),r.getString(2))).collect(); //TODO eliminate list
-//			List<String> affectedLinkSrc = affectedLinks.select("src").javaRDD().map(r -> r.getString(1)).collect();
 			
 			/*
 			 * rebuilds revision edges for all remaining nodes
@@ -128,47 +113,24 @@ public class GraphBuilder implements Serializable {
 			 * 
 			 */
 			
-			List<Node> majorNodeList = majorNodes.sort(org.apache.spark.sql.functions.asc("pageId")).javaRDD().map(r -> new Node(r.getString(0),r.getString(1))).collect();
-			List<Edge> corrRevEdges = new ArrayList<Edge>();
-
-			String pageId;
-			String revId;
-			String prevRevId = "";
-			String prevPageId = "";
+			Dataset<Row> corrRevEdges = majorNodes.withColumnRenamed("id", "src")
+													.join(majorNodes, "pageId")
+													.filter(x -> Integer.parseInt(x.getString(1)) < Integer.parseInt(x.getString(2)))
+													.withColumn("dst", col("id").cast("int"))
+													.drop("id")
+													.groupBy("src", "pageId").min("dst")
+													.withColumnRenamed("min(dst)", "dst")
+													.withColumnRenamed("pageId", "type")
+													.withColumn("dst", col("dst").cast("string"))
+													.select("dst","src","type");
 			
-			for(Node n : majorNodeList) {
-				pageId = n.getPageId();
-				revId = n.getId();
-				
-				if (prevRevId != "") {
-					if (prevPageId.equals(pageId)) {
-						corrRevEdges.add(new Edge(prevRevId,revId,"revision"));
-					}
-				}
-				prevRevId = revId;
-				prevPageId = pageId;
-			}
+			corrRevEdges = spark.createDataFrame(corrRevEdges.javaRDD().map(x -> {return RowFactory.create(x.getString(0), x.getString(1), "revision");}),corrRevEdges.schema());
 			
-			System.out.println("Creating actual Graph..");
-			gf = new GraphFrame(majorNodes, spark.createDataFrame(corrRevEdges, Edge.class));
-			
-
-		
-
-			List<Edge> corrLinkEdges = new ArrayList<Edge>();
-			for(Edge e : affectedLinkEdges) {
-				String src = e.getSrc();
-				
-				String srcPage = dfNodes.filter(x -> x.getString(0).equals(src)).first().getString(1);
-				String newSource = majorNodes.filter(x -> x.getString(1).equals(srcPage)).filter(x -> Integer.parseInt(x.getString(0)) < Integer.parseInt(src)).first().getString(0); //TODO fail! takes always first node
-				Edge corrEdge = new Edge(newSource,e.getDst(),e.getType());
-				corrLinkEdges.add(corrEdge);
-			}
 			
 			List<Edge> allCorrEdges = new ArrayList<>();
-			allCorrEdges.addAll(unaffectedLinksList);
-			allCorrEdges.addAll(corrRevEdges);
-			allCorrEdges.addAll(corrLinkEdges);
+			allCorrEdges.addAll(corrRevEdges.javaRDD().map(r -> new Edge(r.getString(1), r.getString(0),r.getString(2))).collect());
+			allCorrEdges.addAll(unaffectedLinks.javaRDD().map(r -> new Edge(r.getString(1), r.getString(0),r.getString(2))).collect());
+			allCorrEdges.addAll(corrLinkEdges.javaRDD().map(r -> new Edge(r.getString(1), r.getString(0),r.getString(2))).collect());
 			
 			System.out.println("ALLE KNOTEN: "+allNodes.count()+" MINUS "+minorNodes.count()+" = "+majorNodes.count()+" KNOTEN UND "+allCorrEdges.size()+" KANTEN!");
 			writer.writeFile(majorNodeList, allCorrEdges);
